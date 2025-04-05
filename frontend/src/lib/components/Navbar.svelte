f5a7b13418585039890750a62bd09c19ac648b88
Dhruva Srinivas <dhruvas17068@gmail.com>
2025-04-05 14:26:51 -0700

    navbar

---
+++ frontend/src/lib/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import LightDarkToggle from '$lib/components/LightDarkToggle.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<div class="sticky top-0 z-20 w-full border-b bg-background bg-opacity-75 backdrop-blur-md">
+	<div class="container mx-auto flex items-center justify-between px-4 py-3 md:px-6">
+		<Button variant="ghost" href="/" class="text-xl font-bold">KaraokeKit</Button>
+
+		<!-- desktop nav -->
+		<nav class="hidden items-center gap-4 md:flex">
+			<!-- <Button href="/signup" variant="ghost">Sign Up</Button>
+				<Button href="/login" variant="ghost">Log In</Button> -->
+			<LightDarkToggle />
+		</nav>
+	</div>
+</div>
