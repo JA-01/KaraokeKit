6950e2bce43b991a8160fdcc872999bc18e74af7
JA-01 <160474233+JA-01@users.noreply.github.com>
2025-04-05 18:54:30 -0700

    More backend + testing

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,16 @@
 from flask import *
 from flask_cors import CORS, cross_origin
 import os
+from audio_separator.separator import Separator
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'uploads')
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-os.makedirs(os.path.join(os.getcwd(), "processed"), exist_ok=True)
+
+separator = Separator()
+separator.load_model()
 
 @app.route('/helloworld', methods=['GET'])
 @cross_origin()
@@ -17,25 +20,26 @@ def helloworld():
 @app.route('/process', methods=['POST'])
 @cross_origin()
 def process():
-    
     file = request.files['file']
-    print(os.getcwd() + "/static/uploads/" + file.filename)
-    file.save(os.getcwd() + "/static/uploads/" + file.filename)
+    filename = file.filename.split('.')[0]
+    outputfolder = os.path.join(os.getcwd(), "static", filename)
+    os.makedirs(outputfolder, exist_ok=True)
+    save_path = os.path.join(os.getcwd(), "static", filename, file.filename)
+    file.save(save_path)
 
-    #os.mkdir(os.getcwd() + "/processed/" file.filename.split('.')[0])
+    custom_output_names = {
+        "vocals": os.path.join(outputfolder, "vocals"),
+        "instrumental": os.path.join(outputfolder, "instrumental")
+    }
 
+    separator.separate(save_path, custom_output_names)
 
-    response = jsonify({
-            "lyrics": "Never gonna give you up",
-            "audio": file.filename
-        })
-    
-    response.data = file.read()
-
-    response.headers['Content-Type'] = 'audio/mp3'
-    response.headers['Content-Disposition'] = f'attachment; filename="{file.filename}"'
-
-    return response
+    return send_file(
+        custom_output_names["instrumental"] + ".wav",
+        mimetype='audio/wav',
+        as_attachment=True,
+        download_name=file.filename
+    )
     
 
 if __name__ == '__main__':
